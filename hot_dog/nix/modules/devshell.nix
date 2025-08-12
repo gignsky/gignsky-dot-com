fefff8d49c49dac4acb1acd0f6520a6f49182be3
Maxwell Rupp <gig@gignsky.com>
2025-08-12 18:52:44 -0400

    initial hot-dog commit

---
+++ hot_dog/nix/modules/devshell.nix
@@ -0,0 +1,43 @@
+_: {
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "gignsky-dot-com-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          # config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          bacon
+          config.process-compose.cargo-doc-live.outputs.package
+          nil
+          cargo-generate
+          lazygit
+          clippy
+
+          # Formatting tools (replacing treefmt)
+          nixpkgs-fmt
+          rustfmt
+
+          # gigdot programs
+          # inputs.gigdot.packages.${system}.quick-results
+          # inputs.gigdot.packages.${system}.upjust
+          # inputs.gigdot.packages.${system}.upspell
+          # inputs.gigdot.packages.${system}.upflake
+          # inputs.gigdot.packages.${system}.cargo-update
+
+          # Dioxus and such
+          dioxus-cli
+        ];
+        shellHook = ''
+          echo "welcome to the rust development environment for the hot-dog package" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat 2> /dev/null;
+        '';
+      };
+    };
+}
